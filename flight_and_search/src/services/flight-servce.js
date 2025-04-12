4b982d0b221faa07d4edcd1b8aba87a422479301
Ansh Singh <ansh.2022UG1012@iiitranchi.ac.in>
2025-04-12 18:41:43 +0530

    added price filters

--- flight_and_search/src/services/flight-servce.js
+++ flight_and_search/src/services/flight-servce.js
@@ -1,4 +1,5 @@
 const {FlightRepository, AirplaneRepository } = require('../repository/index');
+const { compareTime } = require('../utils/helper');
 
 class FlightService {
 
@@ -10,6 +11,9 @@ class FlightService {
 
     async createFlight(data) {
         try {
+            if(!compareTime(data.arrivalTime, data.departureTime)) {
+                throw{error: 'Arrival time cannot be less than departure time'}
+            }
             const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
             if (!airplane) {
                 throw new Error("Airplane not found for ID: " + data.airplaneId);
@@ -27,7 +31,14 @@ class FlightService {
     }
     
 
-    async getFlightData() {
+    async getAllFlightsData(data) {
+        try {
+            const flights = await this.flightrepository.getAllFlights(data);
+            return flights;
+        } catch (error) {
+            console.log("Something went wrong in service layer");
+            throw{error};
+        }
 
     }
 }
