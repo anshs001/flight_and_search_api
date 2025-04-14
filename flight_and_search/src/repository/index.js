27e9995c2299a9ced654a9acc22462ed4c5b4c2f
Ansh Singh <ansh.2022UG1012@iiitranchi.ac.in>
2025-04-15 00:31:16 +0530

    Added common crud classes for basic crud operations

--- flight_and_search/src/repository/index.js
+++ flight_and_search/src/repository/index.js
@@ -1,9 +1,12 @@
 const AirplaneRepository = require('./airplane-repository');
+const CrudRepository = require('./crud-repository');
 const FlightRepository = require('./flight-repository');
 
 
 module.exports = {
     Cityrepository: require('./city-repository'),
     FlightRepository: require('./flight-repository'),
-    AirplaneRepository: require('./airplane-repository')
+    AirplaneRepository: require('./airplane-repository'),
+    AirportRepository: require('./airport-repository'),
+    CrudRepository: require('./crud-repository')
 }
--- flight_and_search/src/services/index.js
+++ flight_and_search/src/services/index.js
@@ -1,6 +1,11 @@
+const AirportService = require("./airport-service");
 const CityService = require("./city-service");
+const CrudService = require("./crud-service");
 const FlightService=require('./flight-servce');
 module.exports = {
     CityService: require('./city-service'),
-    FlightService: require('./flight-servce')
+    FlightService: require('./flight-servce'),
+    AirportService: require('./airport-service'),
+    CrudService: require('./crud-service'),
+    AirportService:require('./airport-service')
 }
